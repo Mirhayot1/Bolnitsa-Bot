5ca8b620ae552222d3add4392894c94030a1f86b
Mirhayot <mirhayotbekk@gmail.com>
2024-12-16 23:39:35 +0500

    first commit

---
+++ data/locations.py
@@ -0,0 +1,26 @@
+Shops = [
+    ("Kardiologiya markazi", {
+        "lat": 41.55572264423104,
+        "lon": 60.68965454897153
+    }),
+    ("Nano Diagnostics", {
+        "lat": 41.55391837652521,
+        "lon": 60.63560921698024
+    }),
+    ("Viloyat shifoxonasi", {
+        "lat": 41.559251992722686,
+        "lon": 60.63375533715168
+    }),
+    ("Ko'z Mikroxirurgiyasi Markazining Xorazm Filiali.", {
+        "lat": 41.55818199305969,
+        "lon":  60.6334451721107
+    }),
+    ("Bolalar shifoxonasi", {
+        "lat": 41.566120513720044,
+        "lon": 60.62803454165888
+    }),
+    ("Medical Life", {
+        "lat": 41.56630554275545,
+        "lon": 60.630319467886395
+    })
+]
